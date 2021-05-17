b85a4ac8ad59ad32bd6d3199a079083770839711
karnavr <kraval3@uwo.ca>
2021-05-17 18:50:12 -0400

    define fft derivative (method 1)

--- funcs.py
+++ funcs.py
@@ -1,6 +1,18 @@
 import numpy as np
 import scipy as sp
+import matplotlib.pyplot as plt
 
-def fftDeriv():
+def fftDeriv(f, x, order = 1):
 
-    return
+    N = len(x)
+    dx = x[-1] - x[-2]
+
+    k = np.fft.fftfreq(N, dx) * 2 * np.pi
+
+    fhat = np.fft.fft(f)
+
+    derivative = np.fft.ifft(k*1j*fhat).real
+
+    return derivative
+
+    
