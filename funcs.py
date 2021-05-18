181f52ad868eb1f8fc169ec96f1ebbf17eb9523b
karnavr <kraval3@uwo.ca>
2021-05-17 21:31:34 -0400

    add new test case for fft derivative

--- funcs.py
+++ funcs.py
@@ -18,7 +18,7 @@ def fftDeriv(func, domain, method, order = 1):
     elif method == 2:
         fhat = np.fft.fft(func)
 
-        kappa = (2*np.pi/L)*np.arange(-len(domain)/2,len(domain)/2)
+        kappa = (2*np.pi/np.abs(x[-1]-x[0]))*np.arange(-len(domain)/2,len(domain)/2)
         kappa = np.fft.fftshift(kappa) 
 
         dfhat = kappa*fhat*(1j)
@@ -31,6 +31,27 @@ def fftDeriv(func, domain, method, order = 1):
 
     return derivative
 
+
+sys.exit()
+
+## function testing
+
+# Test One 
+
+N = 1000
+x = np.linspace(-10, 10, N)
+
+f = 2*np.sin(x) + 2*np.cos(x)
+derivative = fftDeriv(f, x, 1)
+
+plt.plot(x, 2*np.cos(x) - 2*np.sin(x), label='Exact value', color='#00264D')
+plt.plot(x, derivative, '--', label='Derivative by FFT', color='red')
+plt.legend()
+plt.show()
+
+
+# Test Two
+
 N2 = 1000
 L = 30
 dx = L/N2
@@ -39,30 +60,25 @@ x2 = np.arange(-L/2,L/2, dx, dtype="complex_")
 f2 = np.cos(x2) * np.exp(-np.power(x2,2)/25)
 
 analytical_derivative2 = -(np.sin(x2) * np.exp(-np.power(x2,2)/25 + (2/25)*x2*f2))
-derivative2 = fftDeriv(f2, x2, 2)
+derivative2 = fftDeriv(f2, x2, 3)
 
 plt.plot(x2, analytical_derivative2, label='Exact value', color='#00264D')
 plt.plot(x2, derivative2, '--', label='Derivative by FFT', color='red')
 plt.legend()
 plt.show()
 
-sys.exit()
 
-## function testing
-
-# Method 1
+# Test Three
 
 N = 100
 x = np.linspace(0, 2*np.pi, N, endpoint=False)
 
-f1 = np.sin(2*x) + np.cos(5*x)           # function
+f1 = np.sin(2*x) + np.cos(5*x) 
 
-derivative1 = fftDeriv(f, x)
+derivative1 = fftDeriv(f1, x, 2)
 analytical_derivative1 = 2*np.cos(2*x)-5*np.sin(5*x)
 
-
-
-plt.plot(x,dydx, label='Exact value', color='#00264D')
-plt.plot(x,dydx1, '--', label='Derivative by FFT', color='red')
+plt.plot(x, analytical_derivative1, label='Exact value', color='#00264D')
+plt.plot(x, derivative1, '--', label='Derivative by FFT', color='red')
 plt.legend()
 plt.show()
