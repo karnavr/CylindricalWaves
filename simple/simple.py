98d5ab1646315098ecf8a99c0f32b9e552cf99dd
karnavr <kraval3@uwo.ca>
2021-05-18 19:19:24 -0400

    compute definite integral of first derivative

--- simple/simple.py
+++ simple/simple.py
@@ -9,11 +9,17 @@ N = 1000
 
 x = np.linspace(-L/2, L/2, N)
 
+
 S = np.cos(x) * np.exp(-np.power(x,2)/25)
+S_prime = -1/25 * S/np.cos(x) * (25*np.sin(x) + 2*x*np.cos(x))
+
+definite_integral = np.trapz(S_prime, x=x)
+print(f"Definite Integral {definite_integral}")
 
 
 # plotting
 
 plt.plot(x, S, label='Wavepacket', color='#00264D')
+plt.plot(x, S_prime, label=r'S\'(x)', color='red')
 plt.legend()
 plt.show()
