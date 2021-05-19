76305c0c9ab7168f24cf1e3b90aeee82ada8290f
karnavr <kraval3@uwo.ca>
2021-05-19 16:47:00 -0400

    change parameters input

--- simple/lorenz.py
+++ simple/lorenz.py
@@ -2,15 +2,16 @@ import numpy as np
 import scipy as sp
 import scipy.optimize as so
 import matplotlib.pyplot as plt
+import sys
 
 # Solving for steady states of the Lorenz model/system using Newton's method
 
-def lorenz(u, sigma, r, b):
+def lorenz(u, param):
     """Defines the Lorenz model of ODEs.
 
     Args:
         u (ndarray): x, y and z values
-        sigma, r, b (floats): model parameters 
+        param (ndarray): sigma, r, b (model parameters)
 
     Returns:
         array: values of the lorenz ODEs at given x,y and z
@@ -21,6 +22,11 @@ def lorenz(u, sigma, r, b):
     y = u[1]
     z = u[2]
 
+    # define parameters 
+    sigma = param[0]
+    r = param[1]
+    b = param[2]
+
     # define system of ODEs (eq. 3.32)
     dx = sigma*(y-x)
     dy = r*x - y - x*z
@@ -30,6 +36,8 @@ def lorenz(u, sigma, r, b):
 
 
 initial_guess = np.array([50., 50., 50.])
-roots = so.fsolve(lorenz, initial_guess, args=(10., 28, 8./3.))
+roots, info_dict, _, _ = so.fsolve(lorenz, initial_guess, args=[10., 28., 8./3.], full_output=True)
 
 print(f"Steady states of the lorenz model: {roots}")
+
+print(f"Number of function calls: {info_dict['nfev']}")
