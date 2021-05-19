7b919c683f06dd04acaa2596af4fe049f1d7e71a
karnavr <kraval3@uwo.ca>
2021-05-19 14:34:16 -0400

    solve simple polynomial example

--- simple/lorenz.py
+++ simple/lorenz.py
@@ -1,5 +1,13 @@
 import numpy as np
 import scipy as sp
+import scipy.optimize as so
 import matplotlib.pyplot as plt
 
-# Solving for steady states of the Lorenz system using Newton's method
+# Solving for steady states of the Lorenz model/system using Newton's method
+
+def polynomial(x):
+    return x**2 + 2*x
+
+root = so.fsolve(polynomial, -3)
+
+print(f"Root of polynomial: {root}")
