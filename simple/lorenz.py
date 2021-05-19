c094bff0c65c25bf642286ae5ba329066f74acf1
karnavr <kraval3@uwo.ca>
2021-05-19 17:17:59 -0400

    solve lorenz ODE problem

--- simple/lorenz.py
+++ simple/lorenz.py
@@ -1,8 +1,38 @@
 import numpy as np
-import scipy as sp
 import scipy.optimize as so
+import scipy.integrate as spint
 import matplotlib.pyplot as plt
 import sys
+
+# solving phase space trajectory for the lorenz model 
+
+def lorenztraj(t, state):
+
+    # define x, y and z variables 
+    x = state[0]
+    y = state[1]
+    z = state[2]
+
+    # define system of ODEs (eq. 3.32)
+    dx = 10*(y-x)
+    dy = 28*x - y - x*z
+    dz = x*y - (8/3)*z
+
+    return [dx, dy, dz]
+
+initial_condition = [1., 1., 20.]   # initial condition of x, y and z
+
+# solve ODE problem
+solution = spint.solve_ivp(lorenztraj, [0,14], initial_condition)
+
+# un-pack wanted results
+t = solution.t
+solution_values = solution.y
+x = solution_values[0,:]
+y = solution_values[1,:]
+z = solution_values[2,:]
+
+sys.exit()
 
 # Solving for steady states of the Lorenz model/system using Newton's method
 
