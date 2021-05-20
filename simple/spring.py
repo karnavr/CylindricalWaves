3489104005befb4b401bc6fc5a536cfad3347113
karnavr <kraval3@uwo.ca>
2021-05-19 22:07:01 -0400

    begin spring continuation problem

---
+++ simple/spring.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+## Solving for the equilibrium position of a mass-spring system using Newton's method
+
+
+
+## Observing the change in position with variable k2 constand using numerical continuation
