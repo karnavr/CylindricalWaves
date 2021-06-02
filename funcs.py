ceece61d0dd6ab3bf0c2997060361c9344212d24
karnavr <kraval3@uwo.ca>
2021-06-01 20:22:30 -0400

    define func to compute c0 value

--- funcs.py
+++ funcs.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy as sp
+import scipy.special as sp
 import matplotlib.pyplot as plt
 import sys
 
@@ -31,76 +32,111 @@ def fftDeriv(func, domain, order = 1):
     return derivative
 
 
-sys.exit()
+def initial_c0(L, b, B):
+    """Computes the initial guess for the wave speed, c0.
 
-## function testing
+    Args:
+        L (float): length of half of the solution domain
+        b (float): dimensionless rod radius
+        B (float): dimensionless magnetic bond number
 
-# Test One 
+    Returns:
+        float: c0, initial guess for the wave speed 
+    """
 
-N = 1000
-x = np.linspace(-10, 10, N)
+    # define initial parameters
+    k1 = np.pi/L 
 
-f = 2*np.sin(x) + 2*np.cos(x)
-derivative = fftDeriv(f, x, 1)
+    # bessel functions 
+    def I(domain, order=1):
+        # modified bessel of first kind
+        return sp.iv(order, domain)
+    
+    def K(domain, order=1):
+        # modified bessel of second kind
+        return sp.kn(order, domain)
 
-plt.plot(x, 2*np.cos(x) - 2*np.sin(x), label='Exact value', color='#00264D')
-plt.plot(x, derivative, '--', label='Derivative by FFT', color='red')
-plt.legend()
-plt.show()
+    # parts of the equation 
+    one = 1/k1
+    two = (I(k1)*K(k1*b) - I(k1*b)*K(k1)) / (I(k1*b)*K(k1, order=0) + I(k1, order=0)*K(k1*b))
+    three = k1**2 - 1 + B
+
+    c0 = np.sqrt(one*two*three)
+
+    return c0
+
+
 
 # sys.exit()
 
-# Test Two
+# ## function testing
 
-N2 = 1000
-L = 30
-dx = L/N2
+# # Test One 
 
-x2 = np.arange(-L/2,L/2, dx, dtype="complex_")
-f2 = np.cos(x2) * np.exp(-np.power(x2,2)/25)
+# N = 1000
+# x = np.linspace(-10, 10, N)
 
-analytical_derivative2 = - ((f4/np.cos(x4))*(25*np.sin(x4)+2*x4*np.cos(x4)))/25
-derivative2 = fftDeriv(f2, x2, 3)
+# f = 2*np.sin(x) + 2*np.cos(x)
+# derivative = fftDeriv(f, x, 1)
 
-plt.plot(x2, analytical_derivative2, label='Exact value', color='#00264D')
-plt.plot(x2, derivative2, '--', label='Derivative by FFT', color='red')
-plt.legend()
-plt.show()
+# plt.plot(x, 2*np.cos(x) - 2*np.sin(x), label='Exact value', color='#00264D')
+# plt.plot(x, derivative, '--', label='Derivative by FFT', color='red')
+# plt.legend()
+# plt.show()
+
+# # sys.exit()
+
+# # Test Two
+
+# N2 = 1000
+# L = 30
+# dx = L/N2
+
+# x2 = np.arange(-L/2,L/2, dx, dtype="complex_")
+# f2 = np.cos(x2) * np.exp(-np.power(x2,2)/25)
+
+# analytical_derivative2 = - ((f4/np.cos(x4))*(25*np.sin(x4)+2*x4*np.cos(x4)))/25
+# derivative2 = fftDeriv(f2, x2, 3)
+
+# plt.plot(x2, analytical_derivative2, label='Exact value', color='#00264D')
+# plt.plot(x2, derivative2, '--', label='Derivative by FFT', color='red')
+# plt.legend()
+# plt.show()
 
 
-# Test Three
+# # Test Three
 
-N = 100
-x = np.linspace(0, 2*np.pi, N, endpoint=False)
+# N = 100
+# x = np.linspace(0, 2*np.pi, N, endpoint=False)
 
-f1 = np.sin(2*x) + np.cos(5*x) 
+# f1 = np.sin(2*x) + np.cos(5*x) 
 
-derivative1 = fftDeriv(f1, x, 2)
-analytical_derivative1 = 2*np.cos(2*x)-5*np.sin(5*x)
+# derivative1 = fftDeriv(f1, x, 2)
+# analytical_derivative1 = 2*np.cos(2*x)-5*np.sin(5*x)
 
-plt.plot(x, analytical_derivative1, label='Exact value', color='#00264D')
-plt.plot(x, derivative1, '--', label='Derivative by FFT', color='red')
-plt.legend()
-plt.show()
+# plt.plot(x, analytical_derivative1, label='Exact value', color='#00264D')
+# plt.plot(x, derivative1, '--', label='Derivative by FFT', color='red')
+# plt.legend()
+# plt.show()
 
 
-# Test Four
+# # Test Four
 
-N = 1000
-L = 30
-dx = L/N
+# N = 1000
+# L = 30
+# dx = L/N
 
-x = np.arange(-L/2,L/2, dx, dtype="complex_")
-f = np.cos(x) * np.exp(-np.power(x,2)/25)
+# x = np.arange(-L/2,L/2, dx, dtype="complex_")
+# f = np.cos(x) * np.exp(-np.power(x,2)/25)
 
-# analytical derivatives
-first_derivative = - ((f/np.cos(x))*(25*np.sin(x)+2*x*np.cos(x)))/25
-second_derivative = ((f/np.cos(x))*(100*x*np.sin(x)+(4*(x**2) - 675)*np.cos(x)))/625
+# # analytical derivatives
+# first_derivative = - ((f/np.cos(x))*(25*np.sin(x)+2*x*np.cos(x)))/25
+# second_derivative = ((f/np.cos(x))*(100*x*np.sin(x)+(4*(x**2) - 675)*np.cos(x)))/625
 
-# fft derivative
-derivative = fftDeriv(f, x, 2)
+# # fft derivative
+# derivative = fftDeriv(f, x, 2)
 
-plt.plot(x, second_derivative, label='Exact value', color='#00264D')
-plt.plot(x, derivative, '--', label='Derivative by FFT', color='red')
-plt.legend()
-plt.show()
+# plt.plot(x, second_derivative, label='Exact value', color='#00264D')
+# plt.plot(x, derivative, '--', label='Derivative by FFT', color='red')
+# plt.legend()
+# plt.show()
