da08f3eb738e37d43c0efb639b0b3d9c2951c6c9
karnavr <kraval3@uwo.ca>
2022-06-05 14:35:50 -0400

    create solitary waves notebook

---
+++ solitary waves/solitary.jl
@@ -0,0 +1,33 @@
+### A Pluto.jl notebook ###
+# v0.19.4
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 07690090-e4fe-11ec-2e94-63ce10836175
+md"## Solitary Waves on a Ferrofluid Jet
+
+The propagation of axisymmetric solitary waves on the surface of an otherwise cylindrical ferrofluid jet subjected to a magnetic field is investigated
+
+
+"
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─07690090-e4fe-11ec-2e94-63ce10836175
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
